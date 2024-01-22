6ec4c23f9ca2c1e2941a5c99a4971c9ca3554181
Atilist <71230342+Atilist@users.noreply.github.com>
2024-01-22 19:08:33 +0100

    Plant Items and Vines

--- mods/plants/init.lua
+++ mods/plants/init.lua
@@ -5,5 +5,33 @@ minetest.register_node("plants:roots", {
 	tiles = {"plants_roots.png"},
     drawtype = "allfaces",
 	paramtype = "light",
-	node_hp = 0
+	node_hp = 0,
+	drop = {
+		items = {
+		    {items = {"plants:root"}, inherit_color = true}
+		}
+	}
+})
+
+minetest.register_node("plants:vines", {
+	description = S("Vines"),
+	tiles = {"plants_vines.png"},
+    drawtype = "plantlike",
+	paramtype = "light",
+	node_hp = 0,
+	drop = {
+		items = {
+		    {items = {"plants:fiber"}, inherit_color = true}
+		}
+	}
+})
+
+minetest.register_craftitem("plants:root", {
+    description = "Root",
+    inventory_image = "plants_root.png",
+})
+
+minetest.register_craftitem("plants:fiber", {
+    description = "Plant Fibers",
+    inventory_image = "plants_fiber.png",
 })
