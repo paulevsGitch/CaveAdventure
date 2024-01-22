4fd451ebdeb825dae1f28be07c5ae1e4523c0bf7
paulevsGitch <paulevs@yandex.ru>
2024-01-22 12:37:27 +0300

    Copper blocks and ingot

--- mods/ores/init.lua
+++ mods/ores/init.lua
@@ -29,5 +29,63 @@ local function register_ore(name, description, stone)
 	})
 end
 
+local function register_metal(name, description, hp)
+	local ingot = "ores:" .. name .. "_ingot"
+	local block = "ores:" .. name .. "_block"
+	local tiles = "ores:" .. name .. "_tiles"
+
+	local image = "ores_" .. name .. "_ingot.png"
+	local group = name .. "_ingot_source"
+
+	local groups = {}
+	groups[group] = 1
+
+	minetest.register_craftitem(ingot, {
+		description = S(description .. " Ingot"),
+		inventory_image = image,
+		wield_image = image
+	})
+
+	minetest.register_node(tiles, {
+		description = S(description .. " Tiles"),
+		tiles = {"ores_" .. name .. "_tiles.png"},
+		groups = groups,
+		sounds = sounds,
+		node_hp = hp
+	})
+
+	minetest.register_node(block, {
+		description = S(description .. " Block"),
+		tiles = {"ores_" .. name .. "_block.png"},
+		groups = groups,
+		sounds = sounds,
+		node_hp = hp
+	})
+
+	minetest.register_craft({
+		output = block,
+		recipe = {
+			{ ingot, ingot },
+			{ ingot, ingot }
+		},
+	})
+
+	minetest.register_craft({
+		output = tiles .. " 4",
+		recipe = {
+			{ block, block },
+			{ block, block }
+		},
+	})
+
+	minetest.register_craft({
+		output = ingot .. " 4",
+		type = "shapeless",
+		recipe = { "group:" .. group },
+	})
+end
+
 register_ore("coal_ore", "Coal Ore", "stones:limestone")
 register_ore("copper_ore", "Copper Ore", "stones:limestone")
+
+register_metal("copper", "Copper")
